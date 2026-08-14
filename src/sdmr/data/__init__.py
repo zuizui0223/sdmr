860fa1211a8e993cefbb06c47fc66a387d1e4e00
ZHANG RUIQI <rachelzhang0223@gmail.com>
2026-08-14 19:57:25 +0900

    Resolve current CHELSA v2.1 candidate COGs

--- src/sdmr/data/__init__.py
+++ src/sdmr/data/__init__.py
@@ -1,6 +1,12 @@
 """Reproducible public-data preparation for SDMR."""
 
 from .background import bbox_membership, sample_target_group_background
+from .chelsa import (
+    CHELSA_V21_BASE,
+    build_chelsa_cog_uri,
+    raster_specs_from_chelsa_manifest,
+    resolve_chelsa_manifest,
+)
 from .download import GBIFDownloadResult, load_gbif_download
 from .gbif import (
     GBIFBulkDownloadRequired,
@@ -20,6 +26,7 @@ from .raster import RasterLayerSpec, extract_raster_values, sha256_file
 from .species_gate import species_admission_table
 
 __all__ = [
+    "CHELSA_V21_BASE",
     "GBIFBulkDownloadRequired",
     "GBIFDownloadResult",
     "GBIF_COL_XR_CHECKLIST_KEY",
@@ -30,10 +37,13 @@ __all__ = [
     "RasterLayerSpec",
     "admit_occurrences",
     "bbox_membership",
+    "build_chelsa_cog_uri",
     "extract_raster_values",
     "fetch_occurrence_search",
     "load_gbif_download",
     "match_taxon",
+    "raster_specs_from_chelsa_manifest",
+    "resolve_chelsa_manifest",
     "sample_target_group_background",
     "sha256_file",
     "species_admission_table",
