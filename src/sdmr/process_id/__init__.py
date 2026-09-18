bc68ef09b937b5da7d70dd3821da973ebc74cdab
ZHANG RUIQI <rachelzhang0223@gmail.com>
2026-09-18 23:54:39 +0900

    feat: export SDMR v3 Phase-1 API

--- src/sdmr/process_id/__init__.py
+++ src/sdmr/process_id/__init__.py
@@ -3,6 +3,7 @@
 This namespace is scientifically separate from the frozen Product-A v2.x line.
 """
 
+from .evidence import OccurrenceProcessEvaluation, evaluate_occurrence_processes
 from .registry import FrozenProcessRegistry, freeze_process_registry
 from .states import PROCESS_STATES, classify_process_state
 from .taxonomy import DEFAULT_PLANT_PROCESSES
@@ -10,7 +11,9 @@ from .taxonomy import DEFAULT_PLANT_PROCESSES
 __all__ = [
     "DEFAULT_PLANT_PROCESSES",
     "FrozenProcessRegistry",
+    "OccurrenceProcessEvaluation",
     "PROCESS_STATES",
     "classify_process_state",
+    "evaluate_occurrence_processes",
     "freeze_process_registry",
 ]
