ac1c604e2d41d60dde25a8c8908f2efe5e1b0dcf
ZHANG RUIQI <rachelzhang0223@gmail.com>
2026-09-18 23:45:45 +0900

    feat: export SDMR v3 process registry API

--- src/sdmr/process_id/__init__.py
+++ src/sdmr/process_id/__init__.py
@@ -3,6 +3,14 @@
 This namespace is scientifically separate from the frozen Product-A v2.x line.
 """
 
+from .registry import FrozenProcessRegistry, freeze_process_registry
 from .states import PROCESS_STATES, classify_process_state
+from .taxonomy import DEFAULT_PLANT_PROCESSES
 
-__all__ = ["PROCESS_STATES", "classify_process_state"]
+__all__ = [
+    "DEFAULT_PLANT_PROCESSES",
+    "FrozenProcessRegistry",
+    "PROCESS_STATES",
+    "classify_process_state",
+    "freeze_process_registry",
+]
