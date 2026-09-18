94c0d0e7c3c619c7b085e07ce5a830664652cca9
ZHANG RUIQI <rachelzhang0223@gmail.com>
2026-09-18 23:48:00 +0900

    feat: add SDMR v3 known-truth namespace

---
+++ src/sdmr/process_id/known_truth/__init__.py
@@ -0,0 +1,5 @@
+"""Known-truth worlds for SDMR v3 process identification."""
+
+from .worlds import KNOWN_TRUTH_WORLDS, KnownTruthWorld, simulate_process_world
+
+__all__ = ["KNOWN_TRUTH_WORLDS", "KnownTruthWorld", "simulate_process_world"]
